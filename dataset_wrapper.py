edd896c442b521c5e3deffc2fe488ba6869a2bac
kstepanov7 <62134316+kstepanov7@users.noreply.github.com>
2020-08-12 07:48:04 +0300

    Update dataset_wrapper.py

--- dataset_wrapper.py
+++ dataset_wrapper.py
@@ -12,7 +12,7 @@ import torch
 import torch.utils.data as utils_data
 
 from PIL import Image, ImageOps
-from skimage import io
+from skimage
 
 #https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 class Create_Dataset(utils_data.Dataset):
